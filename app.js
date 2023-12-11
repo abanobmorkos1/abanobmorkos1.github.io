381356671d2eaa3f395d4de978ade915ca406634
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-12-11 13:59:54 -0500

    ui update

--- app.js
+++ app.js
@@ -1,63 +1,79 @@
-// making api request to fetch character data
 
-fetch(`https://rickandmortyapi.com/api/character/`)
-    .then(res => {return res.json()})
-    .then((data)=> list(data.results));
+let count = 1;
 
- 
-        
-        
-        // creating a function using the data (list) pulled from the api   
-        function list(cards){
-        // selecting the div results
-        const $cardSelector = $('#results')
-        // empties out the div
-        $cardSelector.empty()
-        // goes over the "elements" in the list using the forEach loop
-        cards.forEach(element => { 
+// Define the next and prev buttons using jQuery
+const $prev = $('#prev');
+const $next = $('#next');
 
-            // i use the variable i created on line 13 and append the following to make it appear on the HTML
-            $cardSelector.append
-            ( ` <div class = "card"> 
-            <h2> ${element.name}</h2>
-            <img src= ${element.image}> 
-            <span> <h3> Species: <span>${element.species}</span> 
-            <br> Gender: <span>${element.gender}</span> 
-            <br> Status: <span>${element.status}</span>
-            <br> Origin: <span>${element.origin.name}</span>
-            </h3>`)
-           
+// Define the form button using jQuery
+const $submit = $('#submit');
+
+// Event listener for prev button
+$prev.on('click', (e) => {
+    e.preventDefault();
+    count -= 1;
+    getData();
+    searchData()
+})
+
+// Event listener for next button
+$next.on('click', (e) => {
+    e.preventDefault();
+    count += 1;
+    getData();
+    searchData()
+  
+})
+
+// Event listener for search button
+$submit.on('click', (e) => {
+    e.preventDefault(); // Prevent page from reloading
+    searchData();
+})
+
+// Function to fetch character data based on page count
+const getData = () => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
+        .then(res => res.json())
+        .then((data) => {
+            list(data.results);
         });
-    } 
-    
-    // defining the count to use later on as a counter value to go through the pages
-    let count = 1;
+}
 
-    // defining the next and prev buttons using jQuery
-    const $prev = $('input[value="prev"]');
-    const $next = $('input[value="next"]');
-    
-    // event listner for prev button
-    $prev.on('click', (e) =>{
-        e.preventDefault();             // prevent page from loading everytime prev button is hit 
-        count-= 1;                      // the page goes down by 1 
-        getData();                      // Rendering the data
-        
-    })  
-    
-    // event listner for next button
-
-    $next.on('click' , (e) => {
-        e.preventDefault();             // prevent page from loading everytime prev button is hit 
-        count += 1;                     // the page goes up by 1 
-        getData();                      // Rendering the data
-        
+// Function to fetch character data based on search
+const searchData = () => {
+    // Retrieve the values of status and name when the search button is clicked
+    const status = $('#status').val();
+    const name = $('#name').val();
+    fetch(`https://rickandmortyapi.com/api/character/?name=${name}&status=${status}&page=${count}`)
+    .then(res => res.json())
+    .then((data) => {
+        list(data.results);
     })
+}
 
-    // getData is a function that calls the api and its excuted through the next and previous buttons
-    const getData = () => { 
-        fetch(`https://rickandmortyapi.com/api/character/?page=${count}`) 
-        .then(res => {return res.json()})
-        .then((data)=> list(data.results));
+// Function to display character data in the HTML
+function list(cards) {
+    const $cardSelector = $('#results');
+    $cardSelector.empty();
+
+    // Check if 'cards' is defined and not empty
+    if (cards && cards.length > 0) {
+        cards.forEach(element => {
+            $cardSelector.append(`<div class="card">
+                <h2>${element.name}</h2>
+                <img src="${element.image}">
+                <span><h3>Species: <span>${element.species}</span>
+                <br>Gender: <span>${element.gender}</span>
+                <br>Status: <span>${element.status}</span>
+                <br>Origin: <span>${element.origin.name}</span>
+                </h3></div>`);
+        });
+    } else {
+        $cardSelector.append("<h1 class='error'>No more results found.</h1>");
     }
+}
 
+
+// Initial fetch when the page loads
+getData();
